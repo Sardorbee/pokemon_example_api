2eb14f9d1dcb83628834ab7689d9e3b019fae2b4
Sardorbee <sardorgoyibnazarov@gmail.com>
2023-07-19 09:40:41 +0500

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:pokemon_example_api/data/model/pokemon_model.dart';
-import 'package:pokemon_example_api/data/network/api.dart';
-import 'package:pokemon_example_api/ui/detail_pok/pokemon_detail_page.dart';
+
 import 'package:pokemon_example_api/ui/home/home_page.dart';
-import 'package:pokemon_example_api/ui/search_pok/search_pokemon.dart';
-import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
 void main() {
   runApp(const MyApp());
